ddb237a30e333207fc07effd2423e976a5fc2b4b
Asahi <3287039936@qq.com>
2023-07-04 09:34:51 +0800

    编译esp32工程

--- src/main/main.c
+++ src/main/main.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
-
+#include "driver/gpio.h"
+#include "soc/soc.h"
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
 void app_main(void)
 {
-
+    gpio_config_t gpioConfig;
+    gpioConfig.pin_bit_mask = (1ull << 18)|(1ull << 19);
+    gpioConfig.mode = GPIO_MODE_OUTPUT;
+    gpio_config(&gpioConfig);
 }
